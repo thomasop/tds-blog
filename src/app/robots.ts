183ce5130b6b1ef3083a5cb2110fc10820f6e0e8
thomasop <tdss33@hotmail.com>
2024-06-17 15:07:14 +0200

    Edit

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,17 +4,6 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
-      disallow: [
-        "/profile",
-        "/api",
-        "/_next",
-        "/static",
-        "/public",
-        "/dev",
-        "/utilisateur",
-        "/utilisateurs",
-        "/reinitialisation-mot-de-passe/*",
-      ],
       allow: [
         "/",
         "/comment-gerer-le-stress-et-l-anxiete-au-quotidien-conseils-pratiques",
